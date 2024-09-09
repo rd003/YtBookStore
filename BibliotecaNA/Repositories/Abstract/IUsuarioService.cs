7df5887d1fdbba80806d8157cceb2e45b0c0fcd0
Mutizo de J. Maita <kingbadfox3@gmail.com>
2024-09-09 22:07:52 +0200

    feat:(interface) adicionando serviço Abstract e Implementantion

---
+++ BibliotecaNA/Repositories/Abstract/IUsuarioService.cs
@@ -0,0 +1,14 @@
+using BibliotecaNA.Models.Domain;
+using System.Collections.Generic;
+
+namespace BibliotecaNA.Repositories.Abstract
+{
+public interface IUsuarioService
+{
+    bool Add(Usuario usuario);
+    bool Update(Usuario usuario);
+    bool Delete(int id);
+    Usuario FindById(int id);
+    IEnumerable<Usuario> GetAll();
+}
+}
---
+++ BibliotecaNA/Repositories/Implementation/UsuarioService.cs
@@ -0,0 +1,48 @@
+using BibliotecaNA.Models.Domain;
+using BibliotecaNA.Repositories.Abstract;
+
+namespace BibliotecaNA.Repositories.Implementation
+{
+public class UsuarioService : IUsuarioService
+{
+    private readonly BibliotecaContext _context;
+
+    public UsuarioService(BibliotecaContext context)
+    {
+        _context = context;
+    }
+
+    public bool Add(Usuario usuario)
+    {
+        _context.Usuarios.Add(usuario);
+        return _context.SaveChanges() > 0;
+    }
+
+    public bool Update(Usuario usuario)
+    {
+        _context.Usuarios.Update(usuario);
+        return _context.SaveChanges() > 0;
+    }
+
+    public bool Delete(int id)
+    {
+        var usuario = _context.Usuarios.Find(id);
+        if (usuario == null)
+        {
+            return false;
+        }
+        _context.Usuarios.Remove(usuario);
+        return _context.SaveChanges() > 0;
+    }
+
+    public Usuario FindById(int id)
+    {
+        return _context.Usuarios.Find(id);
+    }
+
+    public IEnumerable<Usuario> GetAll()
+    {
+        return _context.Usuarios.ToList();
+    }
+}
+}
